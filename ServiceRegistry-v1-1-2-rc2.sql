f6512b62bd63e4ddb938d685a6ff5deedc3d4096
Georgiy <alxminium@mail.ru>
2026-04-03 22:10:48 +0500

    Скрипт для развёртывания БД

---
+++ ServiceRegistry-v1-1-2-rc2.sql
@@ -0,0 +1,149 @@
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: alxminium_db
+-- ------------------------------------------------------
+-- Server version	8.0.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `objects`
+--
+
+DROP TABLE IF EXISTS `objects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `objects` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `responsible` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `objects`
+--
+
+LOCK TABLES `objects` WRITE;
+/*!40000 ALTER TABLE `objects` DISABLE KEYS */;
+INSERT INTO `objects` VALUES (1,'Офис Центр','ул. Пушкина, 10','Иванов И.И.'),(2,'ЖЭУ-01','УК ТЖХ, центральный офис','Иванов И.И.'),(4,'ЖЭУ-04','УК ТЖХ, центральный офис','Сидоров С.С.'),(5,'ЖЭУ-01 (ГЖФ)','УК ГЖФ, центральный офис','Кузнецов А.Н.'),(6,'ЖЭУ-04 (ГЖФ)','УК ГЖФ, центральный офис','Васильев К.М.'),(7,'ЖЭУ-02','УК ТЖХ, центральный офис','Петров П.П.');
+/*!40000 ALTER TABLE `objects` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `requests`
+--
+
+DROP TABLE IF EXISTS `requests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `requests` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `author` varchar(100) DEFAULT NULL,
+  `section` varchar(100) DEFAULT NULL,
+  `object_id` int DEFAULT NULL,
+  `object_name` varchar(255) DEFAULT NULL,
+  `work_name` varchar(255) DEFAULT NULL,
+  `work_type` varchar(100) DEFAULT NULL,
+  `deadline_days` int DEFAULT NULL,
+  `unit` varchar(50) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `volume` double DEFAULT NULL,
+  `total_cost` decimal(12,2) DEFAULT NULL,
+  `status` varchar(50) DEFAULT 'В очереди',
+  `description` text,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `object_id` (`object_id`),
+  CONSTRAINT `requests_ibfk_1` FOREIGN KEY (`object_id`) REFERENCES `objects` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `requests`
+--
+
+LOCK TABLES `requests` WRITE;
+/*!40000 ALTER TABLE `requests` DISABLE KEYS */;
+INSERT INTO `requests` VALUES (1,'admin','Участок №1',1,'Офис Центр','Ремонт принтера','Техническое обслуживание',7,'шт.',0.00,12,0.00,'В работе','dawdaw','2026-03-31 15:41:25'),(2,'admin','Участок №1',1,'Офис Центр','Ремонт принтера','Техническое обслуживание',7,'шт.',0.00,12,0.00,'В работе','test','2026-03-31 15:45:30'),(3,'admin','Участок №1',1,'Офис Центр','Ремонт принтера','Техническое обслуживание',7,'шт.',0.00,123,0.00,'Выполнена','etst','2026-03-31 18:00:27'),(5,'user1','Участок №1',1,'Офис Центр','Ремонт принтера','Техническое обслуживание',7,'шт.',0.00,2,0.00,'Выполнена','user','2026-04-01 05:46:44'),(6,'admin','Участок №1',1,'Офис Центр','Ремонт принтера','Техническое обслуживание',7,'шт.',0.00,23,0.00,'Выполнена','testtt test','2026-04-01 06:17:40'),(7,'admin','Участок №1',2,'ЖЭУ-01','Разработка прикладного ПО','Разработка ПО',7,'шт.',1500.00,3,4500.00,'Выполнена','test','2026-04-03 05:46:12'),(8,'admin','Участок №1',5,'ЖЭУ-01 (ГЖФ)','Разработка прикладного ПО','Разработка ПО',7,'шт.',1500.00,213,319500.00,'В работе','NOW NOW NOW','2026-04-03 21:03:16');
+/*!40000 ALTER TABLE `requests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `services`
+--
+
+DROP TABLE IF EXISTS `services`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `services` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `work_name` varchar(255) NOT NULL,
+  `work_type` varchar(100) DEFAULT NULL,
+  `description` text,
+  `unit` varchar(50) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `deadline_days` int NOT NULL DEFAULT '7',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `services`
+--
+
+LOCK TABLES `services` WRITE;
+/*!40000 ALTER TABLE `services` DISABLE KEYS */;
+INSERT INTO `services` VALUES (1,'Ремонт принтера','Техническое обслуживание','3',NULL,NULL,7),(2,'Проектирование архитектуры БД','Разработка ПО','Проектирование схем данных...','шт.',1500.00,7),(3,'Разработка прикладного ПО','Разработка ПО','Создание десктопных приложений...','шт.',1500.00,7),(5,'Монтаж видеокамер','Видеонаблюдение','Установка IP камер...','шт.',1500.00,7),(7,'Прокладка кабеля (СКС)','Сетевые работы','Прокладка витой пары...','шт.',1500.00,7),(8,'Профилактика системных блоков','Обслуживание ВТ','Очистка от пыли, замена пасты...','шт.',1500.00,7);
+/*!40000 ALTER TABLE `services` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `login` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(20) NOT NULL,
+  `section` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login` (`login`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin','admin123','Admin','Главный офис'),(2,'user1','user123','User','Участок №1');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-04-03 22:05:31
